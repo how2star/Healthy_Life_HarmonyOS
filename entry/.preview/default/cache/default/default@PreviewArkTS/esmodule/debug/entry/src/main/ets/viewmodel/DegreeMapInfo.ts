d492ddbfde5cbbf9b85f2361b38581d14b1d0192
Travel <140887466+how2star@users.noreply.github.com>
2025-07-10 17:41:49 +0800

    在.gitignore中添加了忽略/entry/build和/entry/.preview

--- entry/.preview/default/cache/default/default@PreviewArkTS/esmodule/debug/entry/src/main/ets/viewmodel/DegreeMapInfo.ts
+++
@@ -1,9 +0,0 @@
-// 单个任务完成率的 图片资源
-export default class DegreeMapInfo {
-    off__200: Resource = { "id": 16777369, "type": 20000, params: [], "bundleName": "com.example.healthy_life", "moduleName": "entry" };
-    on__200: Resource = { "id": 16777362, "type": 20000, params: [], "bundleName": "com.example.healthy_life", "moduleName": "entry" };
-    off__300: Resource = { "id": 16777277, "type": 20000, params: [], "bundleName": "com.example.healthy_life", "moduleName": "entry" };
-    on__300: Resource = { "id": 16777280, "type": 20000, params: [], "bundleName": "com.example.healthy_life", "moduleName": "entry" };
-    off__400: Resource = { "id": 16777274, "type": 20000, params: [], "bundleName": "com.example.healthy_life", "moduleName": "entry" };
-    on__400: Resource = { "id": 16777365, "type": 20000, params: [], "bundleName": "com.example.healthy_life", "moduleName": "entry" };
-}
--- entry/.preview/default/cache/default/default@PreviewArkTS/esmodule/debug/oh_modules/.ohpm/cjcalendar@2.3.3/oh_modules/cjcalendar/src/main/ets/components/components/CJViewModel.ts
+++
@@ -1,8 +0,0 @@
-export enum CJViewModel {
-    // 年视图
-    YEAR = 0,
-    // 月视图
-    MONTH = 1,
-    // 周视图
-    WEEK = 2
-}
